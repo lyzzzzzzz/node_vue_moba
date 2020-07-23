d475d2826b6ccb8c650741ba88804ee087b46b73
liyazhen3 <yazhenli@163.com>
2020-07-23 16:59:23 +0800

    完成基础增产改查

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -9,18 +9,60 @@ module.exports = app => {
     const category = await Category.create(req.body)
     if (category) {
       res.send({
-        msg: "保存成功",
+        msg: "新建成功",
         data: category
       })
     } else {
       res.send({
-        msg: "保存成功"
+        msg: "新建成功"
       })
     }
   })
 
+  //获取全部分类
   router.get('/categories', async (req, res) => {
     const categories = await Category.find()
     res.send(categories)
+  })
+
+
+  //根据id获取分类
+  router.get('/categories/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (category) {
+      res.send(category)
+    } else {
+      res.send({
+        msg: "数据库查找失败"
+      })
+    }
+  })
+ 
+
+  //修改分类
+  router.put('/categories/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (category) {
+      category.name = req.body.name
+      await category.save()
+      res.send({
+        msg: "修改成功",
+        data: category
+      })
+    } else {
+      res.send({
+        msg: "数据库查找失败"
+      })
+    }
+  })
+
+
+  //删除分类
+  router.delete('/categories/:id', async (req, res) =>{
+    const category = await Category.findById(req.params.id)
+    await category.remove()
+    res.send({
+      msg: "删除成功"
+    })
   })
 }
