54b8da034888d0ab6d8e5549ae6a9177f00d2235
liyazhen3 <yazhenli@163.com>
2020-08-12 18:06:31 +0800

    完成topbar

---
+++ web/src/views/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="topbar bg-dark py-2 px-3 d-flex jc-between ai-center">
+      <!-- <div class="d-flex"> -->
+        <img src="../assets/logo.png" height="30" />
+        <div class="ml-2 flex-1">
+          <div class="text-white fs-md">王者荣耀</div>
+          <div class="text-grey fs-sm mt-1">团队成就更多</div>
+        </div>
+      <!-- </div> -->
+      <button class="bg-primary btn" >立即下载</button>
+    </div>
+    <div class="nav" >nav</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
